aff50db803dafef98f7c481f8b17c4a990bbc026
Zach Tratar <ztratar@gmail.com>
2014-02-09 04:14:42 -0800

    Real structure of app coming together

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,6 +1,160 @@
-chrome.extension.onMessage.addListener(
-	function(request, sender, sendResponse) {
-		chrome.pageAction.show(sender.tab.id);
-		sendResponse();
+// Followr Background.js
+
+// -----------------------
+// Backend Logic
+// -----------------------
+
+var backend = {};
+
+// Launch Twitter function
+backend.launchTwitterInBackground = function() {
+	// Store run time in milliseconds
+	chrome.storage.sync.set({
+		'lastRun': (new Date()).getTime()
+	});
+	backend.incrementRunCount();
+
+	chrome.windows.create({
+		url: 'http://twitter.com/?followr=true',
+		width: 240,
+		height: 240,
+		top: 40000,
+		left: 40000,
+		focused: false,
+		type: 'popup'
+	});
+};
+
+backend.incrementRunCount = function(cb) {
+	chrome.storage.sync.get('runCount', function(data) {
+		chrome.storage.sync.set({
+			runCount: (typeof data.runCount === 'number') ? (data.runCount + 1) : 0
+		}, cb);
+	});
+};
+
+// Backend task to get time last run
+backend.getLastRunTime = function(cb) {
+	chrome.storage.sync.get('lastRun', function(data) {
+		cb(data.lastRun);	
+	});
+};
+
+// Backend task to return time left before next run
+backend.getTimeLeftBeforeRun = function(cb) {
+	backend.getLastRunTime(function(lastRunTime) {
+		var currentTime = (new Date()).getTime(),
+			millisecondsDiff = currentTime - lastRunTime,
+			minutesDiff = Math.floor(millsecondsDiff / (1000 * 60));
+		cb(minutesDiff);
+	});
+
+	return true;
+};
+
+backend.getNewTweets = function(data, cb) {
+	var i = 0,
+		returnTweets = [],
+		getNewTweetRecur = function(tweetIter) {
+			var tweet = data.tweets[tweetIter];
+
+			if (tweetIter >= data.tweets.length) {
+				cb(returnTweets);
+			} else {
+				chrome.storage.sync.get('tweet-' + tweet.id, function(tweetInDb) {
+					if (tweetInDb !== true) {
+						returnTweets.push(tweet);
+					}	
+					getNewTweetRecur(tweetIter + 1);	
+				});
+			}
+		};
+
+	if (!data.tweets || !data.tweets.length) {
+		cb([]);
+	}
+	getNewTweetRecur(0);
+
+	return true;
+};
+
+backend.getSearchQueries = function(cb) {
+	chrome.storage.sync.get('searchQueries', function(data) {
+		cb(data.searchQueries);
+	});
+
+	return true;
+};
+
+backend.getMaxQueries = function(cb) {
+	chrome.storage.sync.get('maxQueries', function(data) {
+		cb(data.maxQueries);
+	});
+
+	return true;
+};
+
+backend.setFavorited = function(data, cb) {
+	var storageObj = {};
+	storageObj['tweet-' + data.id] = true;
+	chrome.storage.sync.set(storageObj);
+
+	return true;
+};
+
+backend.setSearchQueries = function(queries, cb) {
+	queries = queries || [];
+	// TODO: Put an interface to this function
+	chrome.storage.sync.set({
+		searchQueries: [
+			'UX Design',
+			'http://news.ycombinator.com',
+			'FB Stock',
+			'Bitcoin'
+		]
+	});
+
+	return true;
+};
+
+backend.setMaxQueries = function(data, cb) {
+	// TODO: Put an interface to this function
+	chrome.storage.sync.set({
+		maxQueries: data
+	});
+
+	return true;
+};
+
+// -----------------------
+// Run
+// -----------------------
+
+// Favorite query every 30 minutes
+setInterval(backend.launchTwitterInBackground, 1000 * 60 * .5);
+
+// Capture calls from the injection script
+chrome.runtime.onMessage.addListener(
+	function(data, sender, sendResponse) {
+		switch(data.message) {	
+			case 'getTimeLeftBeforeRun':
+				return backend.getTimeLeftBeforeRun(sendResponse);
+			case 'getSearchQueries':
+				return backend.getSearchQueries(sendResponse);
+			case 'getNewTweets':
+				return backend.getNewTweets(data.data, sendResponse);
+			case 'getMaxQueries':
+				return backend.getMaxQueries(sendResponse);
+			case 'setFavorited':
+				return backend.setFavorited(data.data, sendResponse);
+			case 'setSearchQueries':
+				return backend.setSearchQueries(data.data, sendResponse);
+			case 'setMaxQueries':
+				return backend.setMaxQueries(data.data, sendResponse);
+			default:
+				return false;
+		}
 	}
 );
+
+backend.setSearchQueries();
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,69 +1,124 @@
 $(function() {
+	var onTwitterCalledWithFollowr = (window.location.search.indexOf('?followr=true') !== -1),
+		maxQueries = 20,
+		twitter = {};
 
-	var query = 'gates%20out',
-		xhr = $.ajax({
-			url: 'https://twitter.com/i/search/timeline?q='+query+'&src=typd&include_available_features=1&include_entities=1&last_note_ts=0',
-			dataType: 'json',
-			success: function(data, d) {
-				var $followr = $('<div class="followr"></div>'),
-					$followrWrap = $('<div class="followr-wrap"></div>'),
-					$state = $('<span id="followr-state"></span>'),
-					$description = $('<p class="state-descript">Favoriting some tweets!</p>');
-					itemHTML = data.inner ? data.inner.items_html : undefined,
-					items = itemHTML ? itemHTML.match(/data-item-id="([0-9]{18})/g) : [],
-					i = 0;
+	// if on Twitter page from followr
+	if (onTwitterCalledWithFollowr) {
 
-				$followr.appendTo($('body'));
-				$followrWrap.appendTo($followr);
-				$description.appendTo($followrWrap);
-				$state.appendTo($followrWrap);
+		// Set up the status interface
+		var $followr = $('<div class="followr"></div>'),
+			$followrWrap = $('<div class="followr-wrap"></div>'),
+			$state = $('<span id="followr-state"></span>'),
+			$description = $('<p class="state-descript">Favoriting some tweets!</p>');
 
-				items = $.map(items, function(item, j) {
-					return ((j % 2) === 0) ? item : undefined;	
-				});
+		$followr.appendTo($('body'));
+		$followrWrap.appendTo($followr);
+		$description.appendTo($followrWrap);
+		$state.appendTo($followrWrap);
 
-				if (items.length) {
-					$state.html('0/' + items.length);
-					for (i = 0; i < items.length; i++) {
-						(function(i) {
-							var id = items[i].replace('data-item-id="', '');
-							setTimeout(function() {
-								$.ajax({
-									url: 'https://twitter.com/i/tweet/favorite',
-									dataType: 'json',
-									type: 'POST',
+		// Set Up Twitter API Calls
+		twitter.authenticity_token = $('input[name="authenticity_token"]').val();
+		twitter.getTweets = function(query, cb) {
+			query = encodeURIComponent(query);
+
+			$.ajax({
+				url: 'https://twitter.com/i/search/timeline?q=' + query + '&src=typd&include_available_features=1&include_entities=1&last_note_ts=0',
+				dataType: 'json',
+				success: function(data, d) {
+					var itemHTML = data.inner ? data.inner.items_html : undefined,
+						items = itemHTML ? itemHTML.match(/data-item-id="([0-9]{18})/g) : [];
+
+					// Parsed html doesn't map propertly, but this is an
+					// easy hack.
+					//
+					// TODO: make the regexp more accurate.
+					items = $.map(items, function(item, j) {
+						item = item.replace('data-item-id="', '');
+						return ((j % 2) === 0) ? item : undefined;	
+					});
+					items.pop();
+
+					if (items.length) {
+						cb(items);
+					} else {
+						window.remove();
+					}
+				}
+			});
+		};
+		twitter.favoriteTweet = function(id, cb) {
+			$.ajax({
+				url: 'https://twitter.com/i/tweet/favorite',
+				dataType: 'json',
+				type: 'POST',
+				data: {
+					'authenticity_token': twitter.authenticity_token,
+					'id': id
+				},
+				success: function(data) {
+					cb(data);
+				}
+			});
+		};
+
+		// Figure out which search query to use
+		chrome.runtime.sendMessage({
+			message: 'getSearchQueries'
+		}, function(searchQueries) {
+			
+			$.each(searchQueries, function(queryIndex, searchQuery) {
+				// Get results of twitter call
+				twitter.getTweets(searchQuery, function(unfilteredTweets) {
+					// Filter through results to make sure favorites not
+					// already called.
+					
+					chrome.runtime.sendMessage({
+						message: 'getNewTweets',
+						data: {
+							tweets: unfilteredTweets
+						}
+					}, function(tweets) {
+						var favoriteTweetIter = function(tweetNum, queryTweetMax) {
+								var tweetId = tweets[tweetNum];
+
+								chrome.runtime.sendMessage({
+									'message': 'setFavorited',
 									data: {
-										'authenticity_token': '05a177e73229f864c161b46fc433d9e7bbe42b34',
-										'id': id
-									},
-									success: function(data) {
-										console.log('favorited', 'http://twitter.com/heyadam/status/' + id);
-										$state.html((i+1)+'/'+items.length);
-										if ((i+1) >= items.length) {
-											$followr.remove();
-										}
+										id: tweetId
 									}
 								});
-							}, i * 700 + Math.random()*300);
-						})(i);
-					}
-				} else {
-					$followrWrap.remove();
-				}
-			}
+
+								twitter.favoriteTweet(tweetId, function(data) {
+									// Add to Already Favorited DB									
+									console.log(tweetNum, '>', tweets.length, searchQuery, searchQueries[searchQueries.length-1]);
+
+									if ((tweetNum >= tweets.length || tweetNum >= queryTweetMax) && searchQuery === searchQueries[searchQueries.length-1]) {
+										window.remove();
+									}
+								});
+							};
+
+						if (!tweets.length) {
+							return false;
+						}
+
+						// Slowly favorite tweets over time and with randomness.
+						for (i = 0; i < tweets.length; i++) {
+							(function() {
+								var j = i,
+									queryTweetMax = Math.floor(maxQueries / searchQueries.length);
+								if (j < queryTweetMax) {
+									setTimeout(function() {
+										$state.html(searchQuery + ': ' + (j+1) + '/' + queryTweetMax);
+										favoriteTweetIter(j, queryTweetMax);	
+									}, j * 7000 + Math.random() * 4000);
+								}
+							})();
+						}
+					});
+				});
+			});
 		});
-
-	chrome.extension.sendMessage({}, function(response) {
-		var readyStateCheckInterval = setInterval(function() {
-			if (document.readyState === "complete") {
-				clearInterval(readyStateCheckInterval);
-
-				// ----------------------------------------------------------
-				// This part of the script triggers when page is done loading
-				console.log("Hello. This message was sent from scripts/inject.js");
-				// ----------------------------------------------------------
-
-			}
-		}, 10);
-	});
+	}
 });
