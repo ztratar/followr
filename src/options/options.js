27019d72c4baea0a0f9a117d7e37022ca138494c
Zach Tratar <ztratar@gmail.com>
2019-09-22 00:36:48 -0700

    Remove checkProtocolTask from options page tracking

--- src/options/options.js
+++ src/options/options.js
@@ -144,5 +144,6 @@ $(function() {
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-48998506-2', 'ztratar.github.io');
+ga('create', 'UA-48998506-2', 'auto');
+ga('set', 'checkProtocolTask', null); // Required to send data
 ga('send', 'pageview', '/options');
